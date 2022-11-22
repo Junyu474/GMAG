5dc3bd29cd569f92573507ab67484fd58a245369
Junyu474 <juch8146@colorado.edu>
2022-11-21 20:16:28 -0700

    use multiprocess to on the 5 band fits files

--- gmag/sdss.py
+++ gmag/sdss.py
@@ -1,4 +1,5 @@
 import warnings
+from multiprocessing import Pool
 from urllib.request import urlopen
 
 import numpy as np
@@ -93,36 +94,45 @@ def __get_galaxy_fits_images_data(run, camcol, field, ra, dec, petroRad_r):
     :param petroRad_r: Petrosian radius, in arcsec
     """
 
-    cutout_images = []
-    r = petroRad_r / 3600  # convert to deg
+    r = petroRad_r / 3600
 
-    for band in ['u', 'g', 'r', 'i', 'z']:
-        url = f"https://dr17.sdss.org/sas/dr17/eboss/photoObj/frames/301/" \
-              f"{run}/{camcol}/frame-{band}-{run:06d}-{camcol}-{field:04d}.fits.bz2"
+    with Pool(5) as p:
+        cutout_images = p.starmap(__process_galaxy_fits_image_data,
+                                  [(run, camcol, field, band, ra, dec, r) for band in 'ugriz'])
 
-        # Read fits file
-        hdu = fits.open(url, cache=False)
-
-        # Read wcs, ignore warnings
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", category=FITSFixedWarning)
-            wcs = WCS(hdu[0].header)
-
-        # Compute cutout size
-        coord = SkyCoord(ra, dec, unit='deg')
-        edge_coord = SkyCoord(ra + r, dec + r, unit='deg')
-        x, y = wcs.world_to_pixel(coord)
-        x_edge, y_edge = wcs.world_to_pixel(edge_coord)
-        # radius is max of x and y, cutout radius is 1.25*radius rounded up to nearest 10
-        radius = max(abs(x - x_edge), abs(y - y_edge))
-        cutout_radius = int(np.ceil(1.25 * radius / 10) * 10)
-
-        # Get cutout, indices in integer
-        min_y, max_y = int(y - cutout_radius), int(y + cutout_radius)
-        min_x, max_x = int(x - cutout_radius), int(x + cutout_radius)
-        cutout_image = hdu[0].data[min_y:max_y, min_x:max_x]
-
-        cutout_images.append(cutout_image)
-
-    # Return cutout image
     return cutout_images
+
+
+def __process_galaxy_fits_image_data(run, camcol, field, band, ra, dec, r):
+    """Get fits image data from url and cutout image,
+    multiprocessing worker function for __get_galaxy_fits_images_data
+    """
+
+    # Get fits image url
+    url = f"https://dr17.sdss.org/sas/dr17/eboss/photoObj/frames/301/" \
+          f"{run}/{camcol}/frame-{band}-{run:06d}-{camcol}-{field:04d}.fits.bz2"
+
+    # Read fits image url into numpy array
+    # Read fits file
+    hdu = fits.open(url, cache=False)
+
+    # Read wcs, ignore warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=FITSFixedWarning)
+        wcs = WCS(hdu[0].header)
+
+    # Compute cutout size
+    coord = SkyCoord(ra, dec, unit='deg')
+    edge_coord = SkyCoord(ra + r, dec + r, unit='deg')
+    x, y = wcs.world_to_pixel(coord)
+    x_edge, y_edge = wcs.world_to_pixel(edge_coord)
+    # radius is max of x and y, cutout radius is 1.25*radius rounded up to nearest 10
+    radius = max(abs(x - x_edge), abs(y - y_edge))
+    cutout_radius = int(np.ceil(1.25 * radius / 10) * 10)
+
+    # Get cutout, indices in integer
+    min_y, max_y = int(y - cutout_radius), int(y + cutout_radius)
+    min_x, max_x = int(x - cutout_radius), int(x + cutout_radius)
+    cutout_image = hdu[0].data[min_y:max_y, min_x:max_x]
+
+    return cutout_image
